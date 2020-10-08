20451309157e086e100d0fe96fbcdd260e2ed38d
Piotr Humin <bardzonigdy4@gmail.com>
2020-10-08 14:01:36 +0200

    add static Header

--- src/components/CompleteNavigation.js
+++ src/components/CompleteNavigation.js
@@ -2,7 +2,7 @@ import React from "react"
 import styled from "styled-components"
 
 import TopNav from "./TopNav"
-// import Header from "./Header"
+import Header from "./Header"
 
 const StyledWrapper = styled.div`
   width: 100%;
@@ -23,7 +23,7 @@ const StyledWrapper = styled.div`
 const CompleteNavigation = () => (
   <StyledWrapper>
     <TopNav></TopNav>
-    {/* <Header></Header> */}
+    <Header></Header>
   </StyledWrapper>
 )
 
--- src/components/header.js
+++ src/components/header.js
@@ -1,57 +1,52 @@
-// import React from "react"
-// import styled from "styled-components"
+import React from "react"
+import styled from "styled-components"
 // import { graphql, useStaticQuery } from "gatsby"
-// import Logo from "./Logo"
-// import Nav from "./Nav"
-// import Hamburger from "./Hamburger"
+import Logo from "./Logo"
+import Nav from "./Nav"
+import Hamburger from "./Hamburger"
 
-// const StyledWrapper = styled.div`
-//   display: flex;
-//   justify-content: space-between;
-//   align-self: flex-start;
-//   min-height: 300px;
-//   z-index: 4;
+const StyledWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-self: flex-start;
+  min-height: 300px;
+  z-index: 4;
 
-//   background-image: url(${({ background }) => background});
-//   background-repeat: no-repeat;
-//   background-size: contain;
-//   background-position-x: center;
-//   padding: 1.8vw 130px 0 130px;
+  background-image: url(${({ background }) => background});
+  background-repeat: no-repeat;
+  background-size: contain;
+  background-position-x: center;
+  padding: 1.8vw 130px 0 130px;
 
-//   @media (max-width: 1200px) {
-//     background-size: cover;
-//     padding: 1.8vw 25px 0 25px;
-//   }
-//   @media (max-width: 640px) {
-//     min-height: 30vh;
-//     background-size: cover;
-//     position: fixed;
+  @media (max-width: 1200px) {
+    background-size: cover;
+    padding: 1.8vw 25px 0 25px;
+  }
+  @media (max-width: 640px) {
+    min-height: 30vh;
+    background-size: cover;
+    position: fixed;
 
-//     top: 0;
-//     left: 0;
-//   }
-//   nav {
-//     /* display: none; */
-//   }
-//   .menu-btn.active + nav {
-//     transform: translateX(0px);
-//   }
-// `
+    top: 0;
+    left: 0;
+  }
 
-// const Header = () => {
-//   const data = useStaticQuery(query)
+  .menu-btn.active + nav {
+    transform: translateX(0px);
+  }
+`
 
-//   return (
-//     <StyledWrapper
-//       className="header"
-//       background={data.allFile.nodes[0].publicURL}
-//     >
-//       <Logo />
-//       <Hamburger />
-//       <Nav />
-//     </StyledWrapper>
-//   )
-// }
+const Header = () => {
+  // const data = useStaticQuery(query)
+
+  return (
+    <StyledWrapper className="header" background="header.svg">
+      <Logo />
+      <Hamburger />
+      <Nav />
+    </StyledWrapper>
+  )
+}
 
 // const query = graphql`
 //   {
@@ -63,4 +58,4 @@
 //   }
 // `
 
-// export default Header
+export default Header
